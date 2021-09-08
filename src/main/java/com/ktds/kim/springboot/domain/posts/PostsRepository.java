338320fd1efe503feb6f9f0dcc45bc6c58678de9
khjhk92 <khjhk92@gmail.com>
2021-09-09 02:18:01 +0900

    3장 p.100 (3.3 완료)

---
+++ src/main/java/com/ktds/kim/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.ktds.kim.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
